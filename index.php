a3ecacd422e4e3a4777993bab4f6eea33c3b9457
pablovasquez321 <70274944+pablovasquez321@users.noreply.github.com>
2020-09-01 22:55:21 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Informate</title>
+	<link rel="stylesheet" type="text/css" href="estilo.css">
+</head>
+<body >
+
+
+
+<div class="tit"> <center><h1> Morfología De Una Flor </h1>></center></div>
+<img src="flor.jpg" class="fondo">
+
+<div class="e1"></div>	
+<div class="e2"></div>	
+<div class="e3"></div>	
+<div class="e4"></div>
+
+
+
+</body>
+</html>
